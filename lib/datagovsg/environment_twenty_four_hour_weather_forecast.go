393e0a1659f87d0712fbb0f130f9b15d2c13e11c
Hafiz Ismail <hafiz@wehavefaces.net>
2016-05-11 13:06:37 +0800

    Modelled response from `https://api.data.gov.sg/v1/environment/24-hour-weather-forecast`

---
+++ lib/datagovsg/environment_twenty_four_hour_weather_forecast.go
@@ -0,0 +1,30 @@
+package datagovsg
+
+type TwentyFourHourWeatherForecast struct {
+	Time    DatetimeRange         `json:"time,omitempty"`
+	Regions RegionWeatherForecast `json:"regions,omitempty"`
+}
+
+type GeneralTwentyFourHourWeatherForecast struct {
+	Forecast         string           `json:"forecast,omitempty"`
+	RelativeHumidity RelativeHumidity `json:"relative_humidity,omitempty"`
+	Temperature      Temperature      `json:"temperature,omitempty"`
+	Wind             Wind             `json:"wind,omitempty"`
+}
+
+type TwentyFourHourWeatherForecastResult struct {
+	UpdateTimestamp string                               `json:"update_timestamp,omitempty"`
+	Timestamp       string                               `json:"timestamp,omitempty"`
+	ValidPeriod     DatetimeRange                        `json:"valid_period,omitempty"`
+	General         GeneralTwentyFourHourWeatherForecast `json:"general,omitempty"`
+	Periods         []TwentyFourHourWeatherForecast      `json:"periods,omitempty"`
+}
+
+type TwentyFourHourWeatherForecastResponse struct {
+	APIInfo APIInfo                               `json:"api_info,omitempty"`
+	Items   []TwentyFourHourWeatherForecastResult `json:"items,omitempty"`
+}
+
+func (resp *TwentyFourHourWeatherForecastResponse) ToGraphQL() interface{} {
+	return resp
+}
